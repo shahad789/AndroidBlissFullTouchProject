4883b41ad5415714127384cfa52c6bb11e62f858
SarahAlzuman <443200744@student.ksu.edu.sa>
2024-03-27 19:20:09 +0300

    sarahs last modification

---
+++ app/src/main/java/com/example/blissfulltouch/myreservationspage.java
@@ -0,0 +1,14 @@
+package com.example.blissfulltouch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class myreservationspage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_myreservationspage);
+    }
+}
