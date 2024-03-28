ba7650cb5d139563cd56e558dbc819e82cea80fc
shahad789 <shahadya7ya@gmail.com>
2024-03-28 17:53:37 +0300

    added java of productdetailsandreview and add location to viewform

---
+++ app/src/main/java/com/example/blissfulltouch/productDetails.java
@@ -0,0 +1,14 @@
+package com.example.blissfulltouch;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class productDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.product_details);
+    }
+}
---
+++ app/src/main/java/com/example/blissfulltouch/review.java
@@ -0,0 +1,12 @@
+package com.example.blissfulltouch;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+public class review extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_review);
+    }
+}
