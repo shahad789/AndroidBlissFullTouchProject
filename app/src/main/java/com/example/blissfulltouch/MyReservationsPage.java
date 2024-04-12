a9dcb6d3da907bf13a64f4663585ec1ac504831a
shooq-10 <Shooq.ev@icloud.com>
2024-04-12 21:02:23 +0300

    from shooq

--- app/src/main/java/com/example/blissfulltouch/MyReservationsPage.java
+++ app/src/main/java/com/example/blissfulltouch/MyReservationsPage.java
@@ -1,20 +1,40 @@
 package com.example.blissfulltouch;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import android.database.Cursor;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.view.MenuItem;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-
-
-import com.example.blissfulltouch.MyDatabaseHelper;
-import com.example.blissfulltouch.ReservationAdapter;
 import java.util.ArrayList;
 
 public class MyReservationsPage extends AppCompatActivity {
+
+    ImageView homeicon;
+    ImageView carticon;
+    ImageView logouticon;
+
 
     private MyDatabaseHelper databaseHelper;
     private RecyclerView recyclerView;
@@ -39,30 +59,50 @@ public class MyReservationsPage extends AppCompatActivity {
         // Set up adapter with the cursor
         adapter = new ReservationAdapter(cursor);
         recyclerView.setAdapter(adapter);
-    }
-    private void deleteAllReservations() {
-        databaseHelper.deleteAllReservations();
-        // Optionally, refresh the RecyclerView or update the UI
-    }
 
-    @Override
-    protected void onDestroy() {
-        // Close the cursor when the activity is destroyed to release resources
-        if (cursor != null) {
-            cursor.close();
-        }
-        super.onDestroy();
-    }
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle home item click
-        if (item.getItemId() == R.id.home) {
-            // Go to the home page
-            Intent homeIntent = new Intent(this, homepage.class);
-            startActivity(homeIntent);
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        // Intent initialization and click listeners should be placed here
+        Intent intent = getIntent();
+        String userEmail = intent.getStringExtra("userEmail");
 
-    }
+        homeicon = findViewById(R.id.homeicon);
+        homeicon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i1 = new Intent(MyReservationsPage.this, homepage.class);
+                i1.putExtra("userEmail", userEmail);
+                startActivity(i1);
+            }
+        });
 
+        carticon = findViewById(R.id.carticon);
+        carticon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Replace MyReservationsPage.class with your desired destination
+                Intent i2 = new Intent(MyReservationsPage.this, MyReservationsPage.class);
+                i2.putExtra("userEmail", userEmail);
+                startActivity(i2);
+            }
+        });
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Log out");
+        builder.setMessage("Are you sure you want to log out?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Intent intent = new Intent(getApplicationContext(), login.class);
+                startActivity(intent);
+            }
+        });
+        builder.setNegativeButton("No", null);
+
+        logouticon = findViewById(R.id.logouticon);
+        logouticon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                builder.show();
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/blissfulltouch/homepage.java
+++ app/src/main/java/com/example/blissfulltouch/homepage.java
@@ -1,31 +1,57 @@
 package com.example.blissfulltouch;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
 public class homepage extends AppCompatActivity {
+
+    ImageView homeicon;
+    ImageView carticon;
+    ImageView logouticon;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
-        ///\\\
-// Find the reservationButton and attach a click listener to it
-        ImageButton reservationButton = findViewById(R.id.reservationButton);
-        reservationButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Redirect to the MyReservationsPage activity
-                Intent intent = new Intent(homepage.this, MyReservationsPage.class);
-                startActivity(intent);
-            }
-        });
-///\\\
 
-
+        // Initialize views and set click listeners
         Button addButton = findViewById(R.id.add);
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,19 +83,63 @@ public class homepage extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        // Search Button
         Button editTextText = findViewById(R.id.editTextTextn);
         editTextText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Redirect to the registration form activity
+                // Redirect to the search page activity
                 Intent intent = new Intent(homepage.this, searchpage.class);
                 startActivity(intent);
             }
         });
 
+        // Intent initialization and click listeners should be placed here
+        Intent intent = getIntent();
+        String userEmail = intent.getStringExtra("userEmail");
+
+        homeicon = findViewById(R.id.homeicon);
+        homeicon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i1 = new Intent(homepage.this, homepage.class);
+                i1.putExtra("userEmail", userEmail);
+                startActivity(i1);
+            }
+        });
+
+        carticon = findViewById(R.id.carticon);
+        carticon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Replace MyReservationsPage.class with your desired destination
+                Intent i2 = new Intent(homepage.this, MyReservationsPage.class);
+                i2.putExtra("userEmail", userEmail);
+                startActivity(i2);
+            }
+        });
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Log out");
+        builder.setMessage("Are you sure you want to log out?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Intent intent = new Intent(getApplicationContext(), login.class);
+                startActivity(intent);
+            }
+        });
+        builder.setNegativeButton("No", null);
+
+        logouticon = findViewById(R.id.logouticon);
+        logouticon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                builder.show();
+            }
+        });
     }
-
-
 }
 
 
